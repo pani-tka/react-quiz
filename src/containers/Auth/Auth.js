e58f1f8abd45a983473ff80c8553a9c55a9154c8
Kateryna Tkachenko <katherinatkachenko@gmail.com>
2020-05-22 12:06:09 +0300

    Added Auth page template

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,43 @@
 import React, {Component} from "react";
+import styles from './Auth.module.css';
+import Button from "../../components/UI/Button/Button";
 
 export default class Auth extends Component {
+
+  loginHandler = () => {
+
+  }
+
+  registerHandler = () => {
+
+  }
+
+  submitHandler = event => {
+    event.preventDefault()
+  }
+
   render() {
     return (
-      <div>
-        <h1>Auth</h1>
+      <div className={styles.Auth}>
+        <div>
+          <h1>Auth</h1>
+          <form onSubmit={this.submitHandler} className={styles.AuthForm}>
+            <input type="text"/>
+            <input type="text"/>
+            <Button
+              type="success"
+              onClick={this.loginHandler}
+            >
+              Log in
+            </Button>
+            <Button
+              type="primary"
+              onClick={this.registerHandler}
+            >
+              Registration
+            </Button>
+          </form>
+        </div>
       </div>
     )
   }
